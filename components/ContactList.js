4d9bddd892c72bb67e998c5b7975d9dc7b71d3c8
Teguh Widodo <teguhwin8@gmail.com>
2021-05-01 00:09:20 +0700

    Contact List Container

---
+++ components/ContactList.js
@@ -0,0 +1,36 @@
+import Contact from './Contact'
+import styles from "./ContactList.module.scss"
+
+const ContactList = () => {
+  return (
+    <div className={styles.container}>
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+    </div>
+  )
+}
+
+export default ContactList
--- components/LeftSide.js
+++ components/LeftSide.js
@@ -6,6 +6,7 @@ import MessageIcon from "@material-ui/icons/Message"
 import MoreVertIcon from "@material-ui/icons/MoreVert"
 import InputBase from "@material-ui/core/InputBase"
 import IconButton from "@material-ui/core/IconButton"
+import ContactList from './ContactList'
 
 const LeftSide = () => {
   return (
@@ -13,26 +14,20 @@ const LeftSide = () => {
       <header className={styles.header}>
         <Avatar className={styles.avatar} />
         <div className={styles.rightHeader}>
-          <IconButton>
+          <IconButton className={styles.iconButton}>
             <DonutLargeIcon />
           </IconButton>
-          <IconButton>
+          <IconButton className={styles.iconButton}>
             <MessageIcon />
           </IconButton>
-          <IconButton>
+          <IconButton className={styles.iconButton}>
             <MoreVertIcon />
           </IconButton>
         </div>
       </header>
       <div className={styles.searchForm}>
         <div className={styles.searchInput}>
-          <IconButton
-            type="submit"
-            className={styles.iconButton}
-            aria-label="search"
-          >
-            <SearchIcon className={styles.searchIcon} />
-          </IconButton>
+          <SearchIcon color="disabled" className={styles.searchIcon} />
           <InputBase
             className={styles.input}
             placeholder="Cari atau mulai chat baru"
@@ -40,7 +35,7 @@ const LeftSide = () => {
           />
         </div>
       </div>
-      {/* ContactList */}
+      <ContactList />
     </>
   )
 }
