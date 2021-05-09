f2117b33ffaf24296dd70a2381cbe2bac0914d45
Teguh Widodo <teguhwin8@gmail.com>
2021-05-10 04:03:52 +0700

    Contact list components

--- components/Contact.js
+++ components/Contact.js
@@ -1,28 +1,25 @@
 import { Avatar } from "@material-ui/core"
 import styles from "./Contact.module.scss"
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
 
-const Contact = () => {
+const Contact = ({ name, message, time }) => {
 	return (
 		<div className={styles.contact}>
-      {/* 1 */}
 			<div className={styles.avatarBox}>
 				<Avatar className={styles.avatar} />
 			</div>
 
-      {/* 2 */}
 			<div className={styles.chatInfo}>
-				<div className={styles.contactName}>Firlian Syaroni</div>
-				<div className={styles.contactMessage}>Pembelian AIGO1H5.083867297505 Hrg=7850 berhasil, Seri Voucher: 9161-5009-6329-9330. Saldo Rp.1176 TRANSAKSI SDH NORMAL KEMBALI</div>
+				<div className={styles.contactName}>{name}</div>
+				<div className={styles.contactMessage}>{message}</div>
 			</div>
 
-      {/* 3 */}
-      <div className={styles.timeStampBox}>
-        <div className={styles.timeStamp}>Kemarin</div>
-        <div className={styles.icon}>
-          <ExpandMoreIcon />
-        </div>
-      </div>
+			<div className={styles.timeStampBox}>
+				<div className={styles.timeStamp}>{time}</div>
+				<div className={styles.icon}>
+					<ExpandMoreIcon />
+				</div>
+			</div>
 		</div>
 	)
 }
--- components/ContactList.js
+++ components/ContactList.js
@@ -4,7 +4,11 @@ import styles from "./ContactList.module.scss"
 const ContactList = () => {
 	return (
 		<div className={styles.container}>
-			<Contact />
+			<Contact
+				name="Teguh Widodo"
+				message="Pembelian AIGO1H5.083867297505 Hrg=7850 berhasil, Seri Voucher: 9161-5009-6329-9330. Saldo Rp.1176 TRANSAKSI SDH NORMAL KEMBALI"
+				time="Kemarin"
+			/>
 		</div>
 	)
 }
